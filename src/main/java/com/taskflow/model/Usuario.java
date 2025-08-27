29369d251ac60cf744f9a4f1040258ef9548fc40
luizsantos7 <lhsantos098@gmail.com>
2025-08-27 08:13:40 -0300

    :sparkles: feat Usuario.model

---
+++ src/main/java/com/taskflow/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.taskflow.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo nome é obrigatório")
+    private String nome;
+
+    @Email
+    @NotNull(message = "O atributo email é obrigatório")
+    private String email;
+
+    @NotNull(message = "O atributo senha é obrigatório")
+    @Size(min = 8, message = "O atributo deve ter no mínimo 8 caracteres")
+    private String senha;
+
+    @Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
+    private String foto;
+}
